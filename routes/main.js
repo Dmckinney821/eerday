070ad431c65318453a494f0fecbebde7fd15d919
Dan McKinney <3dhdaniel@gmail.com>
2020-04-10 12:04:28 -0400

    first

---
+++ routes/main.js
@@ -0,0 +1,14 @@
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+router.get('/', (req, res) => {
+    const data = {
+        greeting: 'Hey Welcome to the show',
+        introduction: 'Developing everyday'
+    }
+
+    res.render('landing', data)
+})
+
+
+module.exports = router;
