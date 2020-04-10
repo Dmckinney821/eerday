d1401d581e1d428783b7599be76ad4786e11d0f0
Dan McKinney <3dhdaniel@gmail.com>
2020-04-10 14:11:48 -0400

    going for a run

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,24 @@
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
-router.get('/', (req, res) => {
+const ProjectController = require('../controllers/ProjectController');
 
+
+
+router.get('/', (req, res) => {
     const data = req.context
-    res.render('landing', data)
+    const projectCtr = new ProjectController()
+    projectCtr.get()
+    .then(projects => {
+        console.log('Projects: ' + JSON.stringify(projects))
+        res.render('landing', data)
+    })
+    .catch(err => {
+        res.send('whoops!' + err.message)
+    })
+
+    
+    
 })
 
 
